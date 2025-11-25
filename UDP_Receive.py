79578de9ce8a0a98f926bd4ef5c05cd972d04c8a
oskar chu <oskar.hyc@gmail.com>
2025-11-25 15:41:54 +0000

    fix: :ambulance: Added byte listener if utef-8 listener couldn't decode

--- UDP_Receive.py
+++ UDP_Receive.py
@@ -10,6 +10,7 @@ This is an example to setup a UDP Listener.
 # First we import our libraries
 import socket   # This library will allow you to communicate over the network
 import sys      # This library will give us some information about your system
+import struct
 
 # First we need to set the IP and PORT we are going to listen to
 # This is the localhost IP address (this machine)
@@ -30,7 +31,27 @@ print("Listening on IP:", UDP_IP, "Port:", UDP_PORT)
 while True:
     #Read data
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    # Print data
-    print ("received message:", data.decode('utf-8')) # As a string (check the ASCII table)
-    print ("received bytes:", list(data)) # As byte values
+    # Try to decode as UTF-8 text first, otherwise interpret as binary numeric payload
+    try:
+        text = data.decode('utf-8')
+        print("received message (utf-8):", text)
+    except UnicodeDecodeError:
+        # Binary payload: show raw bytes and attempt common numeric interpretations
+        b = list(data)
+        print("received bytes:", b)
+        # Interpret single-byte messages as uint8
+        if len(data) == 1:
+            val = struct.unpack('!B', data)[0]
+            print("interpreted as uint8:", val)
+        # Interpret 4-byte messages as float32 (network order) or int32
+        elif len(data) == 4:
+            try:
+                f = struct.unpack('!f', data)[0]
+                print("interpreted as float32:", f)
+            except Exception:
+                i = struct.unpack('!i', data)[0]
+                print("interpreted as int32:", i)
+        else:
+            print("raw binary payload (len=" + str(len(data)) + ")")
+
     print ("from address:", addr) # Print the address of the sender
